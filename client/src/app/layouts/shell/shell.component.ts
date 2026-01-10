71101c22868bb416bd4d0cabc9f87c39e8a6a76c
Thanuja Jinadasa <thanuj.tj@gmail.com>
2026-01-09 23:24:11 -0500

    mobile improvements

--- client/src/app/layouts/shell/shell.component.ts
+++ client/src/app/layouts/shell/shell.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { BottomNavComponent } from '../bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, BottomNavComponent],
+  imports: [RouterOutlet, RouterLink, SidebarComponent, BottomNavComponent],
   templateUrl: './shell.component.html'
 })
 export class ShellComponent {}
